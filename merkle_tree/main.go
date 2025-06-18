0faab6b6864daf2f7319df6a248dedb4b4be0eed
Siddhartha Khuntia <siddharthakhuntia@gmail.com>
2025-06-18 11:30:33 +0530

    Feat: Implement parallel file hashing
    Implements parallel file hashing using goroutines and channels to
    improve performance. Also implements chunking for files larger than 10MB
    to reduce memory usage.

--- merkle_tree/main.go
+++ merkle_tree/main.go
@@ -4,7 +4,10 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"io"
 	"os"
+	"runtime"
+	"sync"
 )
 
 type MerkleNode struct {
@@ -15,11 +18,6 @@ type MerkleNode struct {
 
 type MerkleTree struct {
 	Root *MerkleNode
-}
-
-func hashData(data []byte) []byte {
-	hash := sha256.Sum256(data)
-	return hash[:]
 }
 
 func NewMerkleNode(left, right *MerkleNode, data []byte) *MerkleNode {
@@ -65,16 +63,97 @@ func buildMerkleTree(data [][]byte) *MerkleTree {
 }
 
 func hashFiles(files []string) ([][]byte, error) {
+
+	workers := runtime.NumCPU()
+
+	if len(files) < workers {
+		workers = len(files)
+	}
+
+	jobs := make(chan string, len(files))
+	results := make(chan []byte, len(files))
+
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for job := range jobs {
+				hash, err := hashFile(job)
+				if err != nil {
+					fmt.Println("Error hashing file:", err)
+					continue
+				}
+				results <- hash
+			}
+		}()
+	}
+
+	go func() {
+		defer close(jobs)
+		for _, file := range files {
+
+			jobs <- file
+		}
+	}()
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
 	var data [][]byte
-	for _, file := range files {
+	for result := range results {
+		data = append(data, result)
+	}
+
+	return data, nil
+}
+
+func hashFile(file string) ([]byte, error) {
+	data, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer data.Close()
+
+	stat, err := data.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if stat.IsDir() {
+		return nil, fmt.Errorf("is a directory")
+	}
+
+	hash := sha256.New()
+
+	if stat.Size() <= 10*1024*1024 { // if file is less than 10MB, read the whole file
 		content, err := os.ReadFile(file)
 		if err != nil {
 			return nil, err
 		}
-		hashedContent := hashData(content)
-		data = append(data, hashedContent)
+		hash.Write(content)
+	} else {
+		buffer := make([]byte, 1024*1024) // if file is greater than 10MB, read the file in chunks of 1MB
+		for {
+			n, err := data.Read(buffer)
+			if n > 0 {
+				hash.Write(buffer[:n])
+			}
+
+			if err == io.EOF {
+				break
+			}
+
+			if err != nil {
+				return nil, err
+			}
+		}
 	}
-	return data, nil
+
+	return hash.Sum(nil), nil
 }
 
 func main() {
